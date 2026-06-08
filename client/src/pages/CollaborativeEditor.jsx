ec4cc6f347bbbc6f65c256be90636fec89ec1db0
Palak Maurya <maurya.palak09@gmail.com>
2026-06-08 14:52:49 +0530

    Fix socket URL syntax

--- client/src/pages/CollaborativeEditor.jsx
+++ client/src/pages/CollaborativeEditor.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const socket = io("https://codecollab-server-z5wh.onrender.com/);
+const socket = io("https://codecollab-server-z5wh.onrender.com/");
 
 function CollaborativeEditor() {
   const { roomId } = useParams();
